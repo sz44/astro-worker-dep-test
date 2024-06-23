7492e222eed256a91fa492356b70170d605b689a
sz44 <semion44@gmail.com>
2024-06-23 12:05:31 -0700

    build working with worker! it gets base64 hashed!?

--- src/client/main.js
+++ src/client/main.js
@@ -1,9 +1,12 @@
 console.log("in main.js");
 
-import { pipeline } from '@xenova/transformers';
+const wrkPath = new URL("worker.js", import.meta.url);
+// failed
+// const worker = new Worker("/src/client/worker.js", {type: "module"});
+const worker = new Worker(wrkPath, {type: "module"});
 
-// Allocate a pipeline for sentiment-analysis
-let pipe = await pipeline('sentiment-analysis');
+console.log("import.meta.url: ", import.meta.url);
+console.log("wrkPath: ", wrkPath);
+console.log("wrkPath.toStrint(): ", wrkPath.toString());
 
-let out = await pipe('I hate transformers!');
-console.log(out[0]); // [{'label': 'POSITIVE', 'score': 0.999817686}]
+// const worker = new Worker("/src/client/worker.js", {type: "module"});
