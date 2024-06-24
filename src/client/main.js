787972c77a29abb730856f3aba6dd9e9c378401f
sz44 <semion44@gmail.com>
2024-06-23 18:09:49 -0700

    alternative worker import syntax

--- src/client/main.js
+++ src/client/main.js
@@ -1,9 +1,10 @@
 console.log("in main.js");
 
 import _ from 'lodash';
-
+import MyWorker from './worker?worker';
 // const test = _.sample([1, 2, 3, 4]);
 // console.log("in main.js test: ", test);
 
 // const wrkPath = new URL("worker.js", import.meta.url);
-const worker = new Worker(new URL("worker.js", import.meta.url), {type: "module"});
+// const worker = new Worker(new URL("worker.js", import.meta.url), {type: "module"});
+const worker = new MyWorker();
